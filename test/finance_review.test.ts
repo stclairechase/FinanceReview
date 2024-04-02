db0e89f17e4e1a3ddecc9d65f1d360566e865002
Chase St.Claire <chasestclaire@Chases-MacBook-Air.local>
2024-04-01 20:28:29 -0700

    initalization

---
+++ test/finance_review.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as FinanceReview from '../lib/finance_review-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/finance_review-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new FinanceReview.FinanceReviewStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
